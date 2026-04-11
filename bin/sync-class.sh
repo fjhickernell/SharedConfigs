940f555c96fd8b9f4566154010740b74319a1ecb
Fred J Hickernell <hickernell@iit.edu>
2026-04-11 12:59:10 -0500

    SharedConfigs snapshot on Fred-2019-August-MacBook-Pro-2 at 2026-04-11 12:59:10

--- bin/sync-class.sh
+++ bin/sync-class.sh
@@ -536,7 +536,7 @@ CLASS_REPOS=(
   "$HOME/SoftwareRepositories/MATH476Spring2026"
   "$HOME/SoftwareRepositories/MATH563Spring2026"
   "$HOME/SoftwareRepositories/MATH565Fall2025"
-  # "$HOME/SoftwareRepositories/SIAMUQ26"
+  "$HOME/SoftwareRepositories/SIAMUQ26"
   "$HOME/SoftwareRepositories/MCQMC26"
 )
 
