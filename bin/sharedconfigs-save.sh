7a797ffd5c65037bd745c4aa9050963076fe710b
Fred J. Hickernell <fjhickernell@gmail.com>
2026-05-13 21:30:19 -0500

    SharedConfigs snapshot on Freds-MacBook-Pro at 2026-05-13 21:30:19

--- bin/sharedconfigs-save.sh
+++ bin/sharedconfigs-save.sh
@@ -7,8 +7,10 @@ git add -A
 
 if ! git diff --cached --quiet --exit-code; then
   git commit -m "SharedConfigs snapshot on $(hostname -s) at $(date '+%Y-%m-%d %H:%M:%S')"
-  git push
-  echo "SharedConfigs saved and pushed."
-else
-  echo "No changes to commit."
 fi
+
+git pull --rebase
+
+git push
+
+echo "SharedConfigs synchronized."
